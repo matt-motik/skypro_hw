cd41ad0ba6887702f19a288cf475fca51f0b7770
Matvey Bakirov <mabakirov@gmail.com>
2026-04-10 13:51:09 +0300

    test(widget): add tests and fix validation logic

--- src/widget.py
+++ src/widget.py
@@ -5,6 +5,12 @@ from datetime import datetime
 from src.masks import get_mask_account
 from src.masks import get_mask_card_number
 
+ERROR_MSG_INVALID_FORMAT = (
+    "Ошибка: account_card_number должен быть строкой вида "
+    "'Visa Platinum 1234567890123456' или 'Счет 12345678901234567890'."
+)
+ERROR_MSG_INVALID_DATE_FORMAT = "Ошибка: date_string должна быть ISO формата вида '2024-03-11T02:26:18.671407'."
+
 
 def mask_account_card(account_card_number: str) -> str:
     """Маскирует номер карты или счета в строке.
@@ -22,13 +28,23 @@ def mask_account_card(account_card_number: str) -> str:
         >>>mask_account_card("Счет 12345678901234567890")
         'Счет **7890'
     """
-    number_type, number = account_card_number.rsplit(" ", 1)
+    if not isinstance(account_card_number, str):
+        raise TypeError(ERROR_MSG_INVALID_FORMAT)
+
+    try:
+        number_type, number_str = account_card_number.rsplit(" ", 1)
+    except ValueError:
+        raise ValueError(ERROR_MSG_INVALID_FORMAT)
+
     masked_number: str = ""
+    if not number_str.isdigit():
+        raise TypeError(ERROR_MSG_INVALID_FORMAT)
+
     match number_type.lower():
         case "счет":
-            masked_number = get_mask_account(int(number))
+            masked_number = get_mask_account(int(number_str))
         case _:
-            masked_number = get_mask_card_number(int(number))
+            masked_number = get_mask_card_number(int(number_str))
     return f"{number_type} {masked_number}"
 
 
@@ -48,5 +64,10 @@ def get_date(date_string: str) -> str:
         >>> get_date("2019-07-03T18:35:29.512364")
         '03.07.2019'
     """
-    dt = datetime.fromisoformat(date_string)
+    try:
+        dt = datetime.fromisoformat(date_string)
+    except ValueError:
+        raise ValueError(ERROR_MSG_INVALID_DATE_FORMAT)
+    except TypeError:
+        raise TypeError(ERROR_MSG_INVALID_DATE_FORMAT)
     return dt.strftime("%d.%m.%Y")
