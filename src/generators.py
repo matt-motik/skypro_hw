360a3031a6ce40954bfc5d940c190ee996ab47dc
Bakirov Matvey <mabakirov@gmail.com>
2026-04-18 20:45:55 +0300

    feat: add transaction_descriptions in  generators.py

--- src/generators.py
+++ src/generators.py
@@ -3,6 +3,7 @@
 from typing import Iterator
 
 ERROR_MSG_INVALID_TYPE = "Ошибка: transactions должен быть cписком словарей с банковскими операциями."
+ERROR_MSG_INVALID_CURRENCY_TYPE = "Ошибка: currency должен строкой."
 
 
 def filter_by_currency(transactions: list[dict], currency: str = "USD") -> Iterator[dict]:
@@ -22,6 +23,8 @@ def filter_by_currency(transactions: list[dict], currency: str = "USD") -> Itera
     """
     if not isinstance(transactions, list):
         raise TypeError(ERROR_MSG_INVALID_TYPE)
+    if not isinstance(currency, str):
+        raise TypeError(ERROR_MSG_INVALID_CURRENCY_TYPE)
 
     def check_currency(transaction: dict) -> bool:
         """Проверяет, соответствует ли валюта транзакции заданной."""
@@ -34,9 +37,32 @@ def filter_by_currency(transactions: list[dict], currency: str = "USD") -> Itera
     return filter(check_currency, transactions)
 
 
-def transaction_descriptions(transactions: list[dict]) -> Iterator[dict]:
-    pass
+def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
+    """Возвращает описание каждой операции по очереди.
+
+    Args:
+        transactions: Список словарей с банковскими транзакциями
+
+    Returns:
+        Генератор с описаниями транзакций
+
+    Example:
+         >>> descriptions = transaction_descriptions(transactions)
+         >>> for _ in range(5):
+         ...     print(next(descriptions))
+         >>> Перевод организации
+    """
+    if not isinstance(transactions, list):
+        raise TypeError(ERROR_MSG_INVALID_TYPE)
+
+    for transaction in transactions:
+        if not isinstance(transaction, dict):
+            continue
+        yield transaction.get("description") or "Описание отсутствует"
+
 
 
 def card_number_generator() -> Iterator[dict]:
     pass
+
+
