b61d4f0023616a929a52d497781c24ee90b2a993
Bakirov Matvey <mabakirov@gmail.com>
2026-05-01 20:03:19 +0300

    test: fix tests for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,25 +8,25 @@ from src.utils import read_json_file
 
 
 @pytest.mark.parametrize(
-    "n, data, expected_result",
+    "file_path, data, expected_result",
     [
         (None, None,[]),
         ('', None, []),
         ('wrong/path.json', None,[]),
-        ('temp.json', None, []),
+        ('temp.json', "", []),
         ('temp.json', """{"answer": 42 }""", []),
         ('temp.json', """Non JSON data, or error in JSON""", []),
         ('temp.json', """[ {"id": 0} , {"id": 2} ]""", [ {'id': 0}, {'id': 2} ]),
     ],
 )
 
-def test_read_json_file(n, data, expected_result):
-    if n == 'temp.json':
+def test_read_json_file(file_path, data, expected_result):
+    if file_path == 'temp.json':
         with tempfile.TemporaryDirectory() as tmpdir:
             temp_json = os.path.join(tmpdir,"temp.json")
-            if data:
+            if data is not None:
                 with open(temp_json, "w", encoding="utf-8") as f:
                     f.write(data)
             assert read_json_file(temp_json) == expected_result
     else:
-        assert read_json_file(n) == expected_result
+        assert read_json_file(file_path) == expected_result
