bec628aacbf120b1b461ba0906ac077f05cb1a28
Bakirov Matvey <mabakirov@gmail.com>
2026-05-01 18:58:33 +0300

    feat: add utils.py func read_json_file

---
+++ src/utils.py
@@ -0,0 +1,37 @@
+"""Модуль реализующий функции по работе с операциями."""
+
+import json
+import os.path
+
+from src.decorators import log
+
+
+@log("logs/app.log")
+def read_json_file(filename: str) -> list:
+    """Функция чтения JSON-файла.
+
+    Args:
+        filename: Путь к JSON-файлу транзакций.
+
+    Returns:
+        Список транзакций.
+        Если JSON-файл пустой, содержит не-список или не найден, возвращается пустой список.
+
+    Example:
+        transactions = read_json_file("data/operations.json")
+    """
+    if filename:
+        if os.path.exists(filename):
+            with open(filename, "r", encoding="utf-8") as f:
+                try:
+                    transactions = json.load(f)
+                except json.JSONDecodeError:
+                    print("Error: Invalid JSON data. Return []")
+                    return []
+                if isinstance(transactions, list):
+                    return list(transactions)
+                else:
+                    print("Error: Data isn't a list. Return []")
+                    return []
+    print("Error: Wrong file path. Return []")
+    return []
