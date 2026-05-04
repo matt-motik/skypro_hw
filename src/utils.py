df0c17ced8b950f2cf4b58bf524b7b1aedb22bfa
Bakirov Matvey <mabakirov@gmail.com>
2026-05-04 21:24:34 +0300

    test: add and fix for utils

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,21 @@
 """Модуль реализующий функции по работе с операциями."""
 
 import json
+import logging
 import os.path
 
-from src.decorators import log
 from src.external_api import convert_currency
 
+os.makedirs("logs", exist_ok=True)
 
-@log("logs/app.log")
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler("logs/utils.log", mode='w')
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
 def read_json_file(filename: str) -> list:
     """Функция чтения JSON-файла.
 
@@ -22,24 +30,28 @@ def read_json_file(filename: str) -> list:
 
         >>> transactions = read_json_file("data/operations.json")
     """
+    logger.debug(f"Проверка JSON-файла filename '{filename}' на существование")
     if filename:
         if os.path.exists(filename):
+            logger.debug(f"JSON-файла '{filename}' существует")
+            logger.debug(f"Открываем JSON-файл '{filename}' на чтение")
             with open(filename, "r", encoding="utf-8") as f:
                 try:
+                    logger.debug("Читаем JSON-файл'")
                     transactions = json.load(f)
-                except json.JSONDecodeError:
-                    print("Error: Invalid JSON data. Return []")
+                except json.JSONDecodeError as err:
+                    logger.warning(f"Invalid JSON data. Return []. {str(err)}", exc_info=True)
                     return []
                 if isinstance(transactions, list):
+                    logger.debug("Транзакции успешно прочитаны'")
                     return transactions
                 else:
-                    print("Error: Data isn't a list. Return []")
+                    logger.warning("Data isn't a list. Return [].")
                     return []
-    print("Error: Wrong file path. Return []")
+    logger.warning("Wrong file path. Return []")
     return []
 
 
-@log("logs/app.log")
 def get_amount_in_rub(transaction: dict) -> float:
     """Функция конвертации валюты из USD и EUR в рубли.
 
@@ -57,14 +69,24 @@ def get_amount_in_rub(transaction: dict) -> float:
 
     """
     try:
+        logger.debug(f"Проверка структуры транзакции '{transaction}' на существование полей 'amount' и 'code'")
         amount = float(transaction["operationAmount"]["amount"])
         code = transaction["operationAmount"]["currency"]["code"]
         if not isinstance(code, str):
             raise TypeError("code должен быть строкой.")
     except Exception as err:
+        logger.error(f"Формат транзакции отличается. {str(err)}", exc_info=True)
         raise type(err)(f"Ошибка: Формат транзакции отличается. {str(err)}") from err
 
     if code == "RUB":
+        logger.debug(f"Транзакция в RUB, 'amount = {amount}'")
         return amount
     else:
-        return convert_currency(amount, code)
+        try:
+            logger.debug(f"Транзакция в {code}. Запрашиваем конверсию у внешнего сервиса'")
+            result = convert_currency(amount, code)
+            logger.debug(f"Конверсия в RUB успешна, 'amount = {result}'")
+            return result
+        except Exception as err:
+            logger.error(f"Не удалось выполнить конверсию. {str(err)}", exc_info=True)
+            raise
